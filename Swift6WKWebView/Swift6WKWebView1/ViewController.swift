e333b13105c04deb82f6a9ec36ffe0016459b8ec
biology-student <tsuka0625yskz@gmail.com>
2021-04-21 12:01:54 +0900

    Add Swift6WKWebView

---
+++ Swift6WKWebView/Swift6WKWebView1/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Swift6WKWebView1
+//
+//  Created by Yoshikazu Tsuka on 2021/04/18.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController,WKNavigationDelegate{
+
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    var webView = WKWebView()
+    
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        indicator.isHidden = true
+        
+        //大きさを決める
+        webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - toolBar.frame.size.height)
+        
+        view.addSubview(webView)
+        
+        webView.navigationDelegate = self
+        
+        //URLをロード
+        let url = URL(string: "https://www.kurashiru.com/")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        indicator.layer.zPosition = 2
+    }
+    
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        //ロードが完了した時のデリゲートメソッド
+        indicator.isHidden = true
+        indicator.stopAnimating()
+        
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        
+        //読み込みが開始された時に呼ばれるデリゲートメソッド
+        indicator.isHidden = false
+        indicator.startAnimating()
+    }
+    
+    
+    
+    @IBAction func back(_ sender: Any) {
+        
+        webView.goBack()
+        
+    }
+    
+
+    @IBAction func go(_ sender: Any) {
+        
+        webView.goForward()
+        
+    }
+}
+
