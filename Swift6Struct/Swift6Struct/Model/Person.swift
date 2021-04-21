43c6c76c470740f466d2fdc36438823beca8e08e
biology-student <tsuka0625yskz@gmail.com>
2021-04-21 12:03:07 +0900

    Add Swift6Struct

---
+++ Swift6Struct/Swift6Struct/Model/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  Swift6Struct
+//
+//  Created by Yoshikazu Tsuka on 2021/04/18.
+//
+
+import Foundation
+
+
+struct Person {
+    
+    var name = String()
+    var shumi = String()
+    var movie = String()
+}
