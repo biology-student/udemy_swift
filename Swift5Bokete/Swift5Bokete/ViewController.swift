0aeb93c86b3df562d256923da78cf9f411b430f4
biology-student <tsuka0625yskz@gmail.com>
2021-04-28 12:20:15 +0900

    develop Swift5Bokete 21/4/28 am

--- Swift5Bokete/Swift5Bokete/ViewController.swift
+++ Swift5Bokete/Swift5Bokete/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var commentTextView: UITextView!
     @IBOutlet weak var searchTextField: UITextField!
     
-    
+    var count = 0
     
     
 
@@ -30,21 +30,112 @@ class ViewController: UIViewController {
         PHPhotoLibrary.requestAuthorization { (status) in
             
             switch(status){
-            case .authorized: break
-            case .denied: break
-            case .notDetermined: break
-            case .restricted: break
+                case .authorized: break
+                case .denied: break
+                case .notDetermined: break
+                case .restricted: break
+                case .limited: break
+                @unknown default: break
             }
             
         }
         
-        // Do any additional setup after loading the view.
+        getImages(keyword: "funny")
     }
 
     
     // 検索キーワードの値を元に画像を引っ張ってくる
     // pixabay.com
     
-
+    func getImages(keyword:String){
+        
+        
+        // APIKEY 21355993-9b200e621451764fa2f723712
+        
+        let url = "https://pixabay.com/api/?key=21355993-9b200e621451764fa2f723712&q=\(keyword)"
+        
+        // Alamofireを使ってhttpリクエストを投げる
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+            
+            switch response.result{
+            
+            case .success:
+                
+                
+                let json:JSON = JSON(response.data as Any)
+                var imageString = json["hits"][self.count]["webformatURL"].string
+                
+                
+                if imageString == nil{
+                    
+                    imageString = json["hits"][0]["webformatURL"].string
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                
+                }else{
+                    
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                }
+                
+                
+                
+                
+            
+            case .failure(let error):
+                
+                print(error)
+                
+                
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func nextOdai(_ sender: Any) {
+        
+        count += 1
+        
+        if searchTextField.text == ""{
+            
+            getImages(keyword: "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextField.text!)
+            
+        }
+    }
+    
+    
+    @IBAction func searchAction(_ sender: Any) {
+        
+        self.count = 0
+        
+        if searchTextField.text == ""{
+            
+            getImages(keyword: "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextField.text!)
+            
+        }
+    }
+    
+    
+    
+    @IBAction func next(_ sender: Any) {
+        
+        performSegue(withIdentifier: "next", sender: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let shareVC = segue.destination as? ShareViewController
+        shareVC?.commentString = commentTextView.text
+        shareVC?.resultImage = odaiImageView.image!
+    }
 }
 
