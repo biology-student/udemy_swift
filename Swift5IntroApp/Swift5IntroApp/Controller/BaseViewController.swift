67dbe2b48027070e83122ed2dfc1fbd488b1a692
biology-student <tsuka0625yskz@gmail.com>
2021-04-29 19:49:07 +0900

    Start develop Swift5IntroApp

---
+++ Swift5IntroApp/Swift5IntroApp/Controller/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Swift5IntroApp
+//
+//  Created by Yoshikazu Tsuka on 2021/04/29.
+//
+
+import UIKit
+import SegementSlide
+
+class BaseViewController: SegementSlideViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
